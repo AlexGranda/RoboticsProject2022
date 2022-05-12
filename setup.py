76af33bb1db78c06462cee45901f100ee2dc44a9
Alexandra Granda <alex_a-gc@hotmail.com>
2022-05-12 16:03:35 +0200

    Adds launch folder and first base controller

--- setup.py
+++ setup.py
@@ -10,13 +10,15 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', glob.glob('launch/*.launch')),
+        ('share/' + package_name + '/launch', glob.glob('launch/*.py'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='usi',
-    maintainer_email='usi@todo.todo',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    maintainer_email='granda@usi.ch,silva@usi.ch',
+    description='Robotics 2022 project',
+    license='MIT LIcense',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
