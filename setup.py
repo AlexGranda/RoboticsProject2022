58e0a325712d9014e6e18faf6fb85d7fee35ef51
Alexandra Granda <alex_a-gc@hotmail.com>
2022-05-16 21:38:51 +0200

    Adds main launch file and config for setup.py file

--- setup.py
+++ setup.py
@@ -23,6 +23,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'robomaster_driver = robomaster_ros.robomaster_driver:main',
         ],
     },
 )
